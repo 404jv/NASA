e545474a963e21a6b2b9514bb57648342e348946
Ezever <joaovictorramalho7@gmail.com>
2020-05-05 16:50:38 -0300

    Page de Asteroide

--- mobile/src/pages/Asteroid/index.js
+++ mobile/src/pages/Asteroid/index.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { format } from 'date-fns';
-import { View, Text, FlatList } from 'react-native';
+import { View, Text, FlatList, ScrollView } from 'react-native';
 
 import api from '../../services/api';
 
@@ -21,46 +21,48 @@ export default function Asteroid() {
   }, []);
 
   return (
-    <View style={styles.container}>
-      <Text>Objetos próximos a terra</Text>
-      <Text>Total de objetos: {objects.length}</Text>
-      <FlatList 
-        data={objects}
-        style={styles.objectsList}
-        showsVerticalScrollIndicator={false}
-        keyExtractor={object => String(object.id)}
+    <ScrollView>
+      <View style={styles.container}>
+        <Text style={styles.title}>Objetos próximos a terra</Text>
+        <Text style={styles.objectLen}>Total de objetos: {objects.length}</Text>
+        <FlatList 
+          data={objects}
+          style={styles.objectsList}
+          showsVerticalScrollIndicator={false}
+          keyExtractor={object => String(object.id)}
 
-        renderItem={({item: object}) => (
-          <View style={styles.object}>
-            <Text style={styles.name}>{object.name}</Text>
+          renderItem={({item: object}) => (
+            <View style={styles.object}>
+              <Text style={styles.name}>{object.name}</Text>
 
-            <Text style={styles.strong}>Magnitude</Text>
-            <Text style={styles.span}>{object.absolute_magnitude_h}</Text>
+              <Text style={styles.strong}>Magnitude</Text>
+              <Text style={styles.span}>{object.absolute_magnitude_h}</Text>
 
-            <Text style={styles.strong}>É uma ameaça</Text>
-            <Text style={styles.span}>{object.is_potentially_hazardous_asteroid ? 'Sim' : 'Não'}</Text>
-          
-            <Text style={styles.strong}>Diametro: </Text>
-            <View style={styles.diameters}>
-              <Text style={styles.strong}>KM: </Text>
-              
-              <Text 
-                style={styles.span}
-              > {object.estimated_diameter_kilometers.estimated_diameter_max.toFixed(5)}
-              -{object.estimated_diameter_kilometers.estimated_diameter_min.toFixed(5)}
-              </Text>
-
-              <Text style={styles.strong}>M: </Text>
-              <Text
-                style={styles.span}
-              >{object.estimated_diameter_meters.estimated_diameter_max.toFixed(5)}
-              -{object.estimated_diameter_meters.estimated_diameter_min.toFixed(5)}
-              </Text>
+              <Text style={styles.strong}>É uma ameaça</Text>
+              <Text style={styles.span}>{object.is_potentially_hazardous_asteroid ? 'Sim' : 'Não'}</Text>
             
+              <Text style={styles.strong}>Diametro: </Text>
+              <View style={styles.diameters}>
+                <Text style={styles.strong}>KM: </Text>
+                
+                <Text 
+                  style={styles.span}
+                > {object.estimated_diameter_kilometers.estimated_diameter_max.toFixed(5)}
+                -{object.estimated_diameter_kilometers.estimated_diameter_min.toFixed(5)}
+                </Text>
+
+                <Text style={styles.strong}>M: </Text>
+                <Text
+                  style={styles.span}
+                >{object.estimated_diameter_meters.estimated_diameter_max.toFixed(5)}
+                -{object.estimated_diameter_meters.estimated_diameter_min.toFixed(5)}
+                </Text>
+              
+              </View>
             </View>
-          </View>
-        )}
-      />
-    </View>
+          )}
+        />
+      </View>
+    </ScrollView>
   );
 }
--- mobile/src/pages/Asteroid/style.js
+++ mobile/src/pages/Asteroid/style.js
@@ -1,12 +1,64 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Dimensions } from 'react-native';
+import Constants from 'expo-constants';
+
+let { width, height } = Dimensions.get('window');
 
 export default StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
+    paddingHorizontal: 24,
+    paddingTop: Constants.statusBarHeight + 20,
+    backgroundColor: '#F0F0F5',
   },
 
   actionText: {
     fontSize: 15
+  },
+
+  title: {
+    textAlign: 'center',
+    marginBottom: 25,
+    fontSize: 22,
+    fontWeight: 'bold'
+  },
+
+  objectLen: {
+    color: '#41414D',
+    position: 'relative',
+    right: 80
+  },
+
+  object: {
+    backgroundColor: '#FFF',
+    marginBottom: 15,
+    width: width * 0.8,
+    height: height * 0.4,
+    borderRadius: 8,
+  },
+  
+  name: {
+    textAlign: 'center',
+    fontSize: 18,
+    fontWeight: 'bold'
+  },
+
+  strong: {
+    left: 9,
+    marginBottom: 10,
+    fontWeight: 'bold',
+    color: '#41414d',
+  },
+
+  span: {
+    left: 24,
+    color: '#737380',
+    marginBottom: 10
+  },
+
+  diameters: {
+    left: 15
   }
+
+
 });
