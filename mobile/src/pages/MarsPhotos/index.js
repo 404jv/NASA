66a624169ce993c516ac3e91319b2139bf36fec2
Ezever <joaovictorramalho7@gmail.com>
2020-05-06 21:38:06 -0300

    Page de marsPhoto

--- mobile/src/pages/MarsPhotos/index.js
+++ mobile/src/pages/MarsPhotos/index.js
@@ -1,8 +1,42 @@
-import React from 'react';
-import { Text } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { Text, View, FlatList, Image } from 'react-native';
 
 import styles from './style';
 
+import api from '../../services/api';
+
 export default function MarsPhotos() {
-  return <Text>Mars Photosaaaa</Text>
+  const [marsPhotos, setMarsPhotos] = useState([]);
+
+useEffect(() => {``
+    api.post('/mars', {
+      sol: 2742
+    }).then(res => {
+      setMarsPhotos(res.data);
+    });
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      <FlatList 
+        data={marsPhotos}
+        style={styles.marsPhotosList}
+        showsVerticalScrollIndicator={false}
+        keyExtractor={marsPhoto => String(marsPhoto.id)}
+
+        renderItem={({ item: marsPhoto }) => (
+          <View style={styles.marsPhoto}>
+            <Text style={styles.name}>{marsPhoto.nameCamera}</Text>
+
+            <Image
+              source={{uri: marsPhoto.img_src}}
+              style={styles.image}
+            />
+
+            <Text style={styles.date}>{marsPhoto.earth_date}</Text>
+          </View>
+        )}
+      />
+    </View>
+  );
 }
--- mobile/src/pages/MarsPhotos/style.js
+++ mobile/src/pages/MarsPhotos/style.js
@@ -1,14 +1,43 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Dimensions } from 'react-native';
+import Constants from 'expo-constants';
+
+let { width, height } = Dimensions.get('window');
 
 export default StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#7159c1'
+    paddingHorizontal: 24,
+    paddingTop: Constants.statusBarHeight + 20,
   },
 
-  actionText: {
-    fontSize: 15
+  marsPhoto: {
+    backgroundColor: '#FFF',
+    marginBottom: 40,
+    width: width * 0.9,
+    alignItems: 'center',
+    borderRadius: 8,
+  },
+
+  name: {
+    fontSize: 17,
+    textAlign: 'center',
+    margin: 10,
+    fontWeight: 'bold'
+  },
+
+  image: {
+    left: -5,
+    width: width * 0.8,
+    height: width * 0.4 * 2,
+  },
+
+  date: {
+    fontSize: 17,
+    textAlign: 'center',
+    margin: 10,
+    color: '#737380',
+    fontWeight: 'bold'
   }
 });
